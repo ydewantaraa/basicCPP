6231dc5ed8516b251844b6584e2bdb5c99f56597
Yudha Dewantara <110522695+ydewantaraa@users.noreply.github.com>
2025-02-21 23:28:59 +0700

    Create #07_array.cpp

---
+++ #07_array.cpp
@@ -0,0 +1,25 @@
+// <tipe_data> namaArray[jumlah_elemen];
+// int angka[5] = {1, 2, 3, 4, 5};
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka[3] = {10, 20, 30};
+    cout << "Elemen pertama: " << angka[0] << endl;
+    cout << "Elemen kedua: " << angka[1] << endl;
+    cout << "Elemen ketiga: " << angka[2] << endl;
+    return 0;
+}
+
+// menggunakan loop
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int angka[5] = {1, 2, 3, 4, 5};
+//     for (int i = 0; i < 5; i++) {
+//         cout << "Elemen ke-" << i << ": " << angka[i] << endl;
+//     }
+//     return 0;
+// }
